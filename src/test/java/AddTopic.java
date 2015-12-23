72984d7ad59754861d84ab772f610427fbbb8010
slgu <blackhero98@gmail.com>
2015-12-23 06:50:56 -0500

    add batch test data

---
+++ src/test/java/AddTopic.java
@@ -0,0 +1,91 @@
+import config.Config;
+import db.DbCon;
+import db.Topic;
+import org.bson.Document;
+import util.AwsUtil;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * Created by slgu1 on 12/18/15.
+ */
+public class AddTopic {
+    public static String [] user_uids =
+            new String [] {
+                    "44a97d9e-c1f7-4613-8da9-2d7ac560d072",
+                    "6c90bbd5-cf0f-4626-bcf4-7793ed120c51",
+                    "5d936112-9f90-4eb4-a27d-3b43513ea2b0",
+                    "09a3e62c-f6c3-43f9-a3e1-f854dc49cd1f",
+                    "d0a8676f-ee05-49be-b646-2066e0f74700"
+            };
+    private static double lat_min = 40.785840;
+    private static double lat_max = 40.803548;
+    private static double lon_min = -73.968890;
+    private static double lon_max = -73.946777;
+
+    static Random random = new Random();
+    public static boolean addTopic(String uid, String title, String desc, String video_uid, String img_uid) {
+        Topic topic = new Topic();
+        topic.setTitle(title);
+        topic.setDesc(desc);
+        double a = random.nextDouble();
+        double b = random.nextDouble();
+        topic.setLat(String.valueOf(lat_min + a * (lat_max - lat_min)));
+        topic.setLon(String.valueOf(lon_min + b * (lon_max - lon_min)));
+        topic.setVideo_uid(video_uid);
+        topic.setImg_uid(img_uid);
+        if (topic.insert()) {
+            //store into user list
+            try {
+                DbCon.mongodb.getCollection(Config.UserConnection).findOneAndUpdate(
+                        new Document("uid", uid),
+                        new Document("$push", new Document("topics_list", topic.getUid()))
+                );
+                //to sqs
+                AwsUtil.sendTopicToSQS(topic);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    private static String videodir = "/Users/slgu1/Desktop/video";
+    public static void main(String [] args) {
+        File dir = new File(videodir);
+        int i = 0;
+        for (File f : dir.listFiles()) {
+            if (f.isFile()) {
+                if (f.getName().startsWith("."))
+                    continue;
+                String name = f.getName();
+                if (!name.substring(name.length() - 3, name.length()).equals("mp4"))
+                    continue;
+
+                String [] title_desc = name.substring(0, name.length() - 4).split(",,");
+                /*add video and image*/
+                String video_uid = "";
+                String img_uid = "";
+                try {
+                    String img_path = f.getAbsolutePath().substring(0, f.getAbsolutePath().length() - 4) + ".png";
+                    System.out.println(img_path);
+                    video_uid = AwsUtil.uploadS3(f, "mp4", new FileInputStream(f.getAbsoluteFile()));
+                    img_uid = AwsUtil.uploadS3(new File(img_path),
+                            "png", new FileInputStream(img_path));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                System.out.println(addTopic(user_uids[i % 5], title_desc[0].trim(), title_desc[1].trim(), Config.S3_URL + video_uid, Config.S3_URL + img_uid));
+                ++i;
+            }
+        }
+
+    }
+}
---
+++ src/test/java/AddUser.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+
+import config.Config;
+import db.DbCon;
+import db.User;
+import org.bson.Document;
+import util.AwsUtil;
+import util.HttpUtil;
+import util.Util;
+
+import javax.activation.MimetypesFileTypeMap;
+import javax.servlet.http.Part;
+
+/**
+ * Created by slgu1 on 11/29/15.
+ */
+public class AddUser {
+    public static boolean addUser(HashMap<String, Object> mp) {
+        User user = new User();
+        user.setAddress((String) mp.get("address"));
+        user.setAge((Integer) mp.get("age"));
+        user.setEmail((String) mp.get("email"));
+        user.setSex((Integer) mp.get("sex"));
+        user.setName((String) mp.get("name"));
+        user.setPasswd((String) mp.get("passwd"));
+        user.setNickname((String) mp.get("nickname"));
+        user.setImg_uid((String) mp.get("img_uid"));
+        return user.insert();
+    }
+
+    static String [] names = new String[]{
+            "shenlonggu",
+            "songgao",
+            "kanzhu",
+            "liangjin",
+            "messi",
+    };
+
+    static String [] nick_names = new String []{
+            "longdd",
+            "song",
+            "zhukan",
+            "liangshuai",
+            "small flea"
+    };
+
+    static String [] emails = new String [] {
+            "longdd@gmail.com",
+            "song@gmail.com",
+            "zhukan@gmail.com",
+            "liangshuai@gmail.com",
+            "messi@gmail.com"
+    };
+
+    public static void addFriends(String uid, String other_uid) {
+        //add to friends list set
+        DbCon.mongodb.getCollection(Config.UserConnection).findOneAndUpdate(
+                new Document("uid", uid),
+                new Document("$addToSet", new Document("friends_list", other_uid))
+        );
+        DbCon.mongodb.getCollection(Config.UserConnection).findOneAndUpdate(
+                new Document("uid", other_uid),
+                new Document("$addToSet", new Document("friends_list", uid))
+        );
+        //clear set
+        DbCon.mongodb.getCollection(Config.UserConnection).findOneAndUpdate(
+                new Document("uid", uid),
+                new Document("$pull", new Document("todo_list", other_uid))
+        );
+        DbCon.mongodb.getCollection(Config.UserConnection).findOneAndUpdate(
+                new Document("uid", other_uid),
+                new Document("$pull", new Document("todo_list", uid))
+        );
+    }
+    public static String imgDir = "/Users/slgu1/Desktop/touxiang";
+
+    //add user
+    public static void main(String[] args) {
+        File dir = new File(imgDir);
+        int i = 0;
+        for (File f : dir.listFiles()) {
+            if (f.isFile()) {
+                if (f.getName().startsWith("."))
+                    continue;
+                InputStream io = null;
+                try {
+                    io = new BufferedInputStream(new FileInputStream(f.getAbsoluteFile()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                try {
+                    String key = AwsUtil.uploadS3(f, "png", new FileInputStream(f.getAbsoluteFile()));
+                    HashMap<String, Object> mp = new HashMap<>();
+                    mp.put("name", names[i]);
+                    mp.put("nickname", nick_names[i]);
+                    mp.put("sex", 0);
+                    mp.put("age", 18);
+                    mp.put("passwd", Util.encrypt("123"));
+                    mp.put("address", "Columbia University");
+                    mp.put("email", emails[i]);
+                    mp.put("img_uid", Config.S3_URL + key);
+                    System.out.println(addUser(mp));
+                    ++i;
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+            }
+        }
+    }
+}
